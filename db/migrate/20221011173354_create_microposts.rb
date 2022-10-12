acb4fd0fc78b3e8c465c21d6827d943a20a681dd
david <david.peba123@gmail.com>
2022-10-12 13:00:52 +0200

    New Artibutes Microposts (working)

--- db/migrate/20221011173354_create_microposts.rb
+++ db/migrate/20221011173354_create_microposts.rb
@@ -1,7 +1,9 @@
 class CreateMicroposts < ActiveRecord::Migration[7.0]
   def change
     create_table :microposts do |t|
-      t.text :content
+      t.text :title
+      t.text :url
+      t.text :text
       t.integer :user_id
 
       t.timestamps
