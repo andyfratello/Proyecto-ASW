88a1465e1f82e9a9caa6e66bb27e36ac2523ca6b
Andreu Orensanz <andreu.orensanz@estudiantat.upc.edu>
2022-11-01 20:14:43 +0100

    Tasca #48: Vista d'usuari loguejat. Ja funciona login (cal repassar)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.full_name = auth.info.name   # assuming the user model has a name
-      user.avatar_url = auth.info.image # assuming the user model has an image
-    end
-  end
-
   has_many :microposts
 
   def self.from_omniauth(auth)
