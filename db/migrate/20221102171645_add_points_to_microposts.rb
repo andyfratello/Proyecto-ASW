bc3af7addc4fa27db63766f0a5366f9a2dcc5ed9
Andreu Orensanz <andreu.orensanz@estudiantat.upc.edu>
2022-11-02 19:01:05 +0100

    Tasca #37: Afegit atribut points i nous botons al perfil

---
+++ db/migrate/20221102171645_add_points_to_microposts.rb
@@ -0,0 +1,5 @@
+class AddPointsToMicroposts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :microposts, :points, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_31_115754) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_175840) do
   create_table "microposts", force: :cascade do |t|
     t.text "title"
     t.text "url"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_31_115754) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "points"
   end
 
   create_table "users", force: :cascade do |t|
