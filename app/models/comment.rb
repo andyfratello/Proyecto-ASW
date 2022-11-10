f431bfa1259796dfc8f7bcb4b76c663f116c15b0
david <david.peba123@gmail.com>
2022-11-10 23:22:42 +0100

    bug fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :micropost
   belongs_to :user
   belongs_to :parent, class_name: 'Comment', optional: true
-  has_many :comments, foreign_key: :parent_id
+  has_many :comments, foreign_key: :parent_id, dependent: :destroy
   has_many :comment_likes, dependent: :destroy
 end
