6a421433145e491d0781bd2f310b3166e08d6a1e
david <david.peba123@gmail.com>
2022-11-28 10:53:51 +0100

    Api token fix

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,24 +4,23 @@ class MicropostsController < ApplicationController
   # GET /microposts or /microposts.json
   def index
     api_key = request.headers[:HTTP_X_API_KEY]
-    if api_key.nil?
-      render :json => { "status" => "401", "error" => "No Api key provided."}, status: :unauthorized
-    else
-      @user = User.find_by_api_key(api_key)
-      if @user.nil?
-        render :json => { "status" => "401", "error" => "No User found with the Api key provided."}, status: :unauthorized
+    @microposts = Micropost.all
+
+    sort = params[:sort]
+    type = params[:type]
+    user = params[:user]
+
+    if sort == 'date'
+      @microposts = Micropost.order(created_at: :desc)
+    elsif type == 'ask'
+      @microposts = Micropost.where(url: [nil, ""])
+
+      if api_key.nil?
+        render :json => { "status" => "401", "error" => "No Api key provided." }, status: :unauthorized
       else
-        @microposts = Micropost.all
-
-        sort = params[:sort]
-        type = params[:type]
-        user = params[:user]
-
-        if sort == 'date'
-          @microposts = Micropost.order(created_at: :desc)
-        elsif type == 'ask'
-          @microposts = Micropost.where(url: [nil, ""])
-
+        @user = User.find_by_api_key(api_key)
+        if @user.nil?
+          render :json => { "status" => "401", "error" => "No User found with the Api key provided." }, status: :unauthorized
         elsif user != nil
           @microposts = Micropost.where(user_id: user).order(created_at: :desc)
         end
@@ -30,6 +29,7 @@ class MicropostsController < ApplicationController
 
     end
   end
+
   # GET /microposts/1 or /microposts/1.json
   def show
     @micropost = Micropost.find(params[:id])
@@ -53,7 +53,7 @@ class MicropostsController < ApplicationController
     respond_to do |format|
       if micropost_params[:url] != "" && Micropost.exists?(url: micropost_params[:url])
         @micropost = Micropost.find_by(url: micropost_params[:url])
-        format.html { redirect_to  @micropost}
+        format.html { redirect_to @micropost }
         # CAMBIAR ^ para que redireccione a la vista
         format.json { render json: @micropost.errors, status: :unprocessable_entity }
       end
@@ -65,12 +65,12 @@ class MicropostsController < ApplicationController
         @comment.user = current_user
         @micropost.text = ""
         if @micropost.save && @comment.save
-          format.html { redirect_to  microposts_url(:sort => "date")}
+          format.html { redirect_to microposts_url(:sort => "date") }
           format.json { render :show, status: :created, location: @micropost }
         end
       end
       if @micropost.save
-        format.html { redirect_to  microposts_url(:sort => "date")}
+        format.html { redirect_to microposts_url(:sort => "date") }
         format.json { render :show, status: :created, location: @micropost }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -98,7 +98,7 @@ class MicropostsController < ApplicationController
     @micropost.destroy
 
     respond_to do |format|
-      format.html { redirect_to microposts_url}
+      format.html { redirect_to microposts_url }
       format.json { head :no_content }
     end
   end
@@ -119,16 +119,15 @@ class MicropostsController < ApplicationController
     end
   end
 
-
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_micropost
-      @micropost = Micropost.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def micropost_params
-      params.require(:micropost).permit(:title, :url, :text, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_micropost
+    @micropost = Micropost.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def micropost_params
+    params.require(:micropost).permit(:title, :url, :text, :user_id)
+  end
 end
