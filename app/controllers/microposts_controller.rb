47df5c1e8b65cc8119c3a2942dca5eabdfbdd52d
david <david.peba123@gmail.com>
2022-11-30 21:44:05 +0100

    Errors on create micropost working

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -22,7 +22,7 @@ class MicropostsController < ApplicationController
       # @user = User.find_by_api_key(api_key)
       #if @user.nil?
       #render :json => { "status" => "401", "error" => "No User found with the Api key provided." }, status: :unauthorized
-          @microposts = Micropost.where(user_id: user).order(created_at: :desc)
+      @microposts = Micropost.where(user_id: user).order(created_at: :desc)
     end
 
   end
@@ -49,103 +49,109 @@ class MicropostsController < ApplicationController
 
     if current_user != nil
       @micropost.user_id = current_user.id
+    elsif api_key.nil?
+      render :json => { "status" => "401", "error" => "No Api key provided." }, status: :unauthorized and return
     else
-      #if api_key.nil?
-        #render :json => { "status" => "401", "error" => "No Api key provided." }, status: :unauthorized
-        #else
-        @user = User.find_by_api_key(api_key)
-        #if @user.nil?
-          #render :json => { "status" => "401", "error" => "No User found with the Api key provided." }, status: :unauthorized
-          #else
-          @micropost.user_id = @user.id
-          #end
-        #end
-    end
+      @user = User.find_by_api_key(api_key)
+      if @user.nil?
+        render :json => { "status" => "401", "error" => "No User found with the Api key provided." }, status: :unauthorized and return
+      else
+        @micropost.user_id = @user.id
+          unless url_valid?(micropost_params[:url])
+            render :json => { "status" => "400", "error" => "No valid URL provided" }, status: :bad_request and return
 
-
-    respond_to do |format|
-      if micropost_params[:url] != "" && Micropost.exists?(url: micropost_params[:url])
-        @micropost = Micropost.find_by(url: micropost_params[:url])
-        format.html { redirect_to @micropost }
-        # CAMBIAR ^ para que redireccione a la vista
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
-      end
-
-      if micropost_params[:url] != "" && micropost_params[:text] != ""
-        @comment = Comment.new
-        @comment.text = @micropost.text
-        @comment.micropost = @micropost
-        @comment.user = current_user
-        @micropost.text = ""
-        if @micropost.save && @comment.save
-          format.html { redirect_to microposts_url(:sort => "date") }
-          format.json { render :show, status: :created, location: @micropost }
+          end
         end
       end
-      if @micropost.save
-        format.html { redirect_to microposts_url(:sort => "date") }
-        format.json { render :show, status: :created, location: @micropost }
+
+      respond_to do |format|
+        if micropost_params[:url] != "" && Micropost.exists?(url: micropost_params[:url])
+          @micropost = Micropost.find_by(url: micropost_params[:url])
+          format.html { redirect_to @micropost }
+          # CAMBIAR ^ para que redireccione a la vista
+          format.json { render json: @micropost.errors, status: :unprocessable_entity }
+        end
+
+        if micropost_params[:url] != "" && micropost_params[:text] != ""
+          @comment = Comment.new
+          @comment.text = @micropost.text
+          @comment.micropost = @micropost
+          @comment.user = current_user
+          @micropost.text = ""
+          if @micropost.save && @comment.save
+            format.html { redirect_to microposts_url(:sort => "date") }
+            format.json { render :show, status: :created, location: @micropost }
+          end
+        end
+        if @micropost.save
+          format.html { redirect_to microposts_url(:sort => "date") }
+          format.json { render :show, status: :created, location: @micropost }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @micropost.errors, status: :unprocessable_entity }
+        end
+      end
+
+    end
+
+    # PATCH/PUT /microposts/1 or /microposts/1.json
+    def update
+      respond_to do |format|
+        if @micropost.update(micropost_params)
+          format.html { redirect_to micropost_url(@micropost) }
+          format.json { render :show, status: :ok, location: @micropost }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @micropost.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    # DELETE /microposts/1 or /microposts/1.json
+    def destroy
+      @micropost.destroy
+
+      respond_to do |format|
+        format.html { redirect_to microposts_url }
+        format.json { head :no_content }
+      end
+    end
+
+    def like
+      @micropost.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.json { head :no_content }
+      end
+    end
+
+    def unlike
+      @micropost.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.json { head :no_content }
+      end
+    end
+
+    def url_valid?(url)
+      url = URI.parse(url) rescue false
+      url.kind_of?(URI::HTTP) || url.kind_of?(URI::HTTPS)
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_micropost
+      @micropost = Micropost.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def micropost_params
+      if current_user != nil
+        params.require(:micropost).permit(:title, :url, :text, :user_id)
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+        params.permit(:title, :url, :text)
       end
     end
 
   end
-
-  # PATCH/PUT /microposts/1 or /microposts/1.json
-  def update
-    respond_to do |format|
-      if @micropost.update(micropost_params)
-        format.html { redirect_to micropost_url(@micropost) }
-        format.json { render :show, status: :ok, location: @micropost }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /microposts/1 or /microposts/1.json
-  def destroy
-    @micropost.destroy
-
-    respond_to do |format|
-      format.html { redirect_to microposts_url }
-      format.json { head :no_content }
-    end
-  end
-
-  def like
-    @micropost.save
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.json { head :no_content }
-    end
-  end
-
-  def unlike
-    @micropost.save
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_micropost
-    @micropost = Micropost.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def micropost_params
-    if current_user != nil
-      params.require(:micropost).permit(:title, :url, :text, :user_id)
-    else
-      params.permit(:title, :url, :text)
-    end
-  end
-
-end
