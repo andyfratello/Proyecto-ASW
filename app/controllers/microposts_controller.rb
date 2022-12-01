2644b6adaa36fc30dce8e3965d20b9c662e6c454
Davidhf <david.herrero.faura@estudiantat.upc.edu>
2022-12-02 00:50:03 +0100

    PUT /comments.json works

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -104,6 +104,9 @@ class MicropostsController < ApplicationController
           end
         end
       end
+      if @micropost.url != "" && !params[:text].nil?
+        render :json => { "status" => "400", "error" => "Cannot fill the text field of a Micropost with URL" }, status: :bad_request and return
+      end
       respond_to do |format|
         if @micropost.update(micropost_params)
           format.html { redirect_to micropost_url(@micropost) }
